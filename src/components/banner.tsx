dcb662c2343e9e3bd5a08d2e0c29d671c1a52b13
anguyen9630 <72194699+anguyen9630@users.noreply.github.com>
2023-04-21 22:31:47 +1000

    Style Barebones

---
+++ src/components/banner.tsx
@@ -0,0 +1,26 @@
+import Logo from '/public/circles_logo.svg'
+import { GiHamburgerMenu } from 'react-icons/gi'
+
+export const BannerContent = () => {
+    
+    return (
+        <header className="fixed top-0 left-0 right-0 backdrop-blur flex w-full h-24 shadow-sm shadow-black">
+            <div className="flex w-1/3 items-center justify-start pl-3">
+                <button className='p-3 hover:bg-slate-900 rounded-2xl items-center justify-center active:bg-slate-950 duration-200'>
+                    <GiHamburgerMenu className='w-8 h-8'/>
+                </button>
+            </div>
+            <div className="flex w-1/3 items-center justify-center">
+                <button className='shadow-md  shadow-black rounded-full'>
+                    <Logo className='w-14 h-14 invert'/>
+                </button>
+            </div>
+            <div className="flex w-1/3 justify-end items-center pr-3  ">
+                <button className='hover:bg-slate-900 w-20 h-14 rounded-2xl font-bold active:bg-slate-950 duration-200'>
+                    Log In
+                </button>
+                
+            </div>
+        </header>
+    );
+};

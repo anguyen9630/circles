5187c85c4cb4867294c9a33773ad04d0ad4b8fc0
anguyen9630 <72194699+anguyen9630@users.noreply.github.com>
2023-04-28 01:42:29 +1000

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,15 +3,6 @@ import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import DiscordProvider from "next-auth/providers/discord"
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "err",
-      clientSecret: process.env.GITHUB_SECRET || "err",
-    }),
-    // ...add more providers here
-  ],
-}
+import { authOptions } from "~/server/auth";
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
